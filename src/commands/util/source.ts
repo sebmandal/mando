539e48782c293c1044d45fdfbf1579a0ecf29791
sebmandal <sebastian.mandal@icloud.com>
2021-07-19 04:49:53 +0200

    added source code command

---
+++ src/commands/util/source.ts
@@ -0,0 +1,29 @@
+import newEmbed from "../../core/customEmbed";
+import { Command } from "../../core/customTypes";
+import commands from "../../core/commandRegistry";
+
+const HelpCommand: Command = {
+	name: "source",
+	description: "Sends back the source code link",
+	usage: "source",
+	run: async (client: any, message: any, args: string[]): Promise<any> => {
+		return await message.channel.send(
+			newEmbed(
+				message,
+				"Source",
+				[
+					{
+						name: "Mando's Source",
+						value:
+							"[ooOoO fancy hyperlink](https://github.com/sebmandal/mando)",
+						inline: false,
+					},
+				],
+				undefined,
+				"https://github.com/sebmandal/mando"
+			)
+		);
+	},
+};
+
+export default HelpCommand;
--- src/core/commandRegistry.ts
+++ src/core/commandRegistry.ts
@@ -4,6 +4,7 @@ import genius from "../commands/api/genius";
 import food from "../commands/api/food";
 import stop from "../commands/voice/stop";
 import play from "../commands/voice/play";
+import source from "../commands/util/source";
 import help from "../commands/util/help";
 
-export default [covid, shorten, genius, food, stop, play, help];
+export default [covid, shorten, genius, food, play, stop, source, help];
